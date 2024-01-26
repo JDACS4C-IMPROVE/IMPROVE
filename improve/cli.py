c5f881fdf316cd531ee3c115ca4eb5f0a9b9132b
wilke <wilke@anl.gov>
2024-01-25 23:27:48 -0600

    Working on creating config

--- improve/cli.py
+++ improve/cli.py
@@ -1,7 +1,10 @@
 import logging
 import argparse
 import os
+
 from candle import parse_from_dictlist
+from improve import config as BaseConfig
+
 
 
 class CLI:
@@ -14,24 +17,26 @@ class CLI:
     
         self.parser=argparse.ArgumentParser(description='IMPROVE Command Line Parser')
         self.logger=logging.getLogger('CLI')
+        self.args = None # Placeholder for args from argparse
+        self.params = None # dict of args
         
         # set logger level
         self.logger.setLevel(logging.INFO)
         self.logger.setLevel("DEBUG")
 
         # Set default options
-        self.parser.add_argument('-i', '--input_dir', metavar='DIR', type=dir, dest="input_dir",
+        self.parser.add_argument('-i', '--input_dir', metavar='DIR', type=str, dest="input_dir",
                                   default=os.getenv("IMPROVE_INPUT_DIR" , "./"), 
                                   help='Base directory for input data. Default is IMPROVE_DATA_DIR or if not specified current working directory. All additional input pathes will be relative to the base input directory.')
-        self.parser.add_argument('-o', '--output_dir', metavar='DIR', type=dir, dest="output_dir",
+        self.parser.add_argument('-o', '--output_dir', metavar='DIR', type=str, dest="output_dir",
                                   default=os.getenv("IMPROVE_OUTPUT_DIR" , "./"), 
                                   help='Base directory for output data. Default is IMPROVE_OUTPUT_DIR or if not specified current working directory. All additional relative output pathes will be placed into the base output directory.')
         self.parser.add_argument('--log_level', metavar='LEVEL', type=str, dest="log_level", 
                                   choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "NOTSET"],
                                   default="WARNING", help="Set log levels. Default is WARNING. Levels are:\
                                       DEBUG, INFO, WARNING, ERROR, CRITICAL, NOTSET") 
-        self.parser.add_argument('-cfg', '--config_file', metavar='INI_FILE', dest="log_level", 
-                                  type=argparse.FileType('r'),
+        self.parser.add_argument('-cfg', '--config_file', metavar='INI_FILE', dest="config_file", 
+                                  type=str,
                                   default=None, help="Config file in INI format.") 
                                   
 
@@ -39,26 +44,40 @@ class CLI:
     def set_command_line_options(self,options=[]):
         self.logger.debug("Setting Command Line Options")
         parse_from_dictlist( options , self.parser)
-        self.parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                                 help='an integer for the accumulator')
+       
 
         
 
     def get_command_line_options(self):
 
         # set standard options
-        args = self.parser.parse_args()
-
-
-
-        pass
+        self.args = self.parser.parse_args()
+        self.params = vars(self.args)
 
     def _check_option(self,option) -> bool:
         pass
     
 
-    def config():
-        pass
+    def config(self, section) -> BaseConfig :
+        cfg=BaseConfig.Config()
+        if self.params['config_file']:
+            if os.path.isfile(self.params['config_file']) :
+                self.logger.info('Loading Config from %s', self.params['config_file'])
+                cfg.file = self.params['config_file']
+                cfg.load_config()
+            else:
+                self.logger.critical("Can't load Config from %s", self.params['config_file'])
+        else: 
+            self.logger.debug('No config file')
+
+        for s in cfg.config.items():
+            print(s)
+        for k in self.params :
+            (value,error) = cfg.param(section=section, key=k , value=self.params[k])
+            print(value)
+        
+        return cfg
+
 
     def initialize_parameters( self,
                               pathToModelDir,
@@ -73,3 +92,10 @@ if __name__ == "__main__":
     defaults=[{ 'action' : 'store' , 'choices' : [ 'A' , 'B' , 'C' ] , 'type' : str , 'name' : "dest" }]
     cli.set_command_line_options(options=defaults)
     cli.get_command_line_options()
+    print(vars(cli.args))
+    cfg=cli.config("Preprocess")
+   
+   
+    for k in cli.params :
+        print(k , cli.params[k])
+    print(cfg.dict(section="Preprocess"))
