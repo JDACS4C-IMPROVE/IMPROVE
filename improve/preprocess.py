05ddf4cfe74fbf56da0aec228ea1565ccfa309af
wilke <wilke@anl.gov>
2024-01-30 09:10:47 -0600

    Preprocess stub

---
+++ improve/preprocess.py
@@ -0,0 +1,17 @@
+import os
+from .config import Config
+
+class Params:
+
+
+
+class Preprocess(Config):
+
+    def __init__(self) -> None:
+        pass
+
+    def add_params
+    
+
+if __name__ == "__main__":
+    p=Preprocess()
