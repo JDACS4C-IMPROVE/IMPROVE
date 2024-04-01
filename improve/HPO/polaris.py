90f54563b53b7663358dd45e11b56f597ec6def4
wilke <wilke@anl.gov>
2024-04-01 16:44:09 -0500

    Debugging

---
+++ improve/HPO/polaris.py
@@ -0,0 +1,77 @@
+from parsl.config import Config
+
+# PBSPro is the right provider for Polaris:
+from parsl.providers import PBSProProvider
+# The high throughput executor is for scaling to HPC systems:
+from parsl.executors import HighThroughputExecutor
+# You can use the MPI launcher, but may want the Gnu Parallel launcher, see below
+from parsl.launchers import MpiExecLauncher # USE the MPIExecLauncher
+# address_by_interface is needed for the HighThroughputExecutor:
+from parsl.addresses import address_by_interface
+
+
+user_opts = {
+    "worker_init":      f"source /path/to/your/virtualenv/bin/activate; cd {run_dir}", # load the environment where parsl is installed
+    "scheduler_options":"#PBS -l filesystems=home:eagle:grand" , # specify any PBS options here, like filesystems
+    "account":          "YOURPROJECT",
+    "queue":            "debug-scaling",
+    "walltime":         "1:00:00",
+    "nodes_per_block":  3, # think of a block as one job on polaris, so to run on the main queues, set this >= 10
+}
+
+checkpoints = get_all_checkpoints(run_dir)
+print("Found the following checkpoints: ", checkpoints)
+
+config = Config(
+        executors=[
+            HighThroughputExecutor(
+                label="htex",
+                available_accelerators=4, # if this is set, it will override other settings for max_workers if set
+                max_workers_per_node=4, # Set as many workers as there are GPUs because we want one worker to use 1 GPU
+                address=address_by_interface("bond0"),
+                cpu_affinity="block-reverse",
+                prefetch_capacity=0,
+                start_method="spawn",  # Needed to avoid interactions between MPI and os.fork
+                provider=PBSProProvider(
+                    launcher=MpiExecLauncher(bind_cmd="--cpu-bind", overrides="--depth=64 --ppn 1"),
+                    account=user_opts["account"],
+                    queue=user_opts["queue"],
+                    select_options="ngpus=4",
+                    # PBS directives (header lines): for array jobs pass '-J' option
+                    scheduler_options=user_opts["scheduler_options"],
+                    # Command to be run before starting a worker, such as:
+                    worker_init=user_opts["worker_init"],
+                    # number of compute nodes allocated for each block
+                    nodes_per_block=user_opts["nodes_per_block"],
+                    init_blocks=1,
+                    min_blocks=0,
+                    max_blocks=1, # Can increase more to have more parallel jobs
+                    cpus_per_node=user_opts["cpus_per_node"],
+                    walltime=user_opts["walltime"]
+                ),
+            ),
+        ],
+        retries=2,
+        app_cache=True,
+)
+
+
+@python_app
+def hello_python (message):
+    return 'Hello %s' % message
+
+@bash_app
+def hello_bash(message, stdout='hello-stdout'):
+    return 'echo "Hello %s" ; env ' % message
+
+
+with parsl.load(local_htex):
+    # invoke the Python app and print the result
+    print(hello_python('World (Python)').result())
+
+    # invoke the Bash app and read the result from a file
+    hello_bash('World (Bash)').result()
+
+print('Bash app wrote to hello-stdout:')
+with open('hello-stdout', 'r') as f:
+    print(f.read())
