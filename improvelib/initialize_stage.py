3cd2a42c9c185fdfc318a941201bcb7cbface251
Sara Jones <91564739+jonesse3@users.noreply.github.com>
2024-06-21 11:10:01 -0400

    Update and rename initalize_stage.py to initialize_stage.py
    fix typo

--- improvelib/initialize_stage.py
+++ improvelib/initialize_stage.py
@@ -29,7 +29,7 @@ improve_infer_params = [
 ]
 
 
-def initalize_benchmark(filepath, defmodel, additional_definitions):
+def initialize_benchmark(filepath, defmodel, additional_definitions):
     bmk = Benchmark(
             filepath=filepath,
             defmodel=defmodel,
@@ -52,13 +52,13 @@ def determine_stage_specific_params(filename, filepath, application, defmodel, m
     #to do -- error handing
     if "preprocess" in filename:
         additional_definitions = improve_preprocess_params + app_preprocess_params + model_preprocess_params
-        params = initalize_benchmark(filepath, defmodel, additional_definitions)
+        params = initialize_benchmark(filepath, defmodel, additional_definitions)
     elif "train" in filename:
         additional_definitions = improve_train_params + app_train_params + model_train_params
-        params = initalize_benchmark(filepath, defmodel, additional_definitions)
+        params = initialize_benchmark(filepath, defmodel, additional_definitions)
     elif "infer" in filename:
         additional_definitions = improve_infer_params + app_infer_params + model_infer_params
-        params = initalize_benchmark(filepath, defmodel, additional_definitions)
+        params = initialize_benchmark(filepath, defmodel, additional_definitions)
     else:
         print("File name does not contain 'preprocess', 'train', or 'infer'!")
     return params
