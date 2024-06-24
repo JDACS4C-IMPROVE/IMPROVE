fced1aa1d7265d3545a1a3e3233d80c9bc35ad08
Andreas Wilke <wilke@anl.gov>
2024-06-24 12:51:16 -0500

    Removed options

--- improvelib/preprocess.py
+++ improvelib/preprocess.py
@@ -63,10 +63,6 @@ class Preprocess(Config):
       
         # check usage of self.options
         self.options = Preprocess.preprocess_options
-
-
-        self.cli.parser.add_argument('--supplemental', nargs=2, action='append', metavar=('TYPE', 'FILE'), 
-                              type=str, help='Supplemental data FILE and TYPE. FILE is in INPUT_DIR.')
        
         # Set subparser for benchmark and file
         # subparsers=self.cli.parser.add_subparsers(dest='subparser_name')
@@ -81,41 +77,7 @@ class Preprocess(Config):
         benchmark.add_argument("--splits_dir", type=str, default="splits" , help="Dir name that contains files that store split ids of the y data file.")
         benchmark.add_argument("--supplement_dir", type=str, default=None , help="Dir name that contains supplemental data.")
 
-        drp=benchmark.add_argument_group('Drug Response Prediction', 'Options for drug response prediction benchmark data')
-       
-        # drp.add_argument("--raw_data_dir", type=str, dest="input_dir" , help="Data dir name that stores the raw data, same as input_dir. The directory includes x data, y data, and splits.")
-  
-        # drp.add_argument("--x_data_file", type=str, default="x.parquet" , help="File name for features. Default is x.parquet.")
-        # drp.add_argument("--y_data_file", type=str, default="y.parquet" , help="File name for target. Default is y.parquet.")
-        drp.add_argument("--pred_col_name_suffix", 
-                        type=str, default="_pred" , 
-                        help="Suffix to add to a column name in the y data file to identify predictions made by the model (e.g., if y_col_name is 'auc', then a new column that stores model predictions will be added to the y data file and will be called 'auc_pred').")
-        drp.add_argument("--train_split_file", default="train_split.txt" , 
-                         help="File name for the file that contains training split ids. Default is train_split.txt.")
-        drp.add_argument("--val_split_file", default="val_split.txt" , 
-                         help="File name for the file that contains validation split ids. Default is val_split.txt.")
-        drp.add_argument("--test_split_file", default="test_split.txt" , 
-                         help="File name for the file that contains test split ids. Default is test_split.txt.")    
-        # drp.add_argument("--ml_data_outdir" , dest="output_dir", type=str, 
-                        #  help="Output dir name for the preprocessed data. Same as output_dir. Default is ${input_dir}.")
-        drp.add_argument("--feature_data_format", type=str, default="parquet" ,
-                        help="Output format for the preprocessed data. Default is parquet.")
-        drp.add_argument("--y_col_name", type=str, default="auc" , 
-                         help="Column name in the y data file (e.g., response.tsv), that represents the target variable that the model predicts. In drug response prediction problem it can be IC50, AUC, and others.")
-        drp.add_argument("--y_data_suffix" , default="y_data" , 
-                         help="Suffix to compose file name for storing true y dataframe. Default is y_data.")
-        drp.add_argument("--y_data_files", nargs='+', type=str, default=['response.tsv'] ,
-                         help="List of files that contain the y (prediction variable) data. Example: ['response.tsv']")
-        # x_data_canc_files , input is a list of tuples, each tuple has two elements, the first element is the file name and the second element is a comma separated list of gene system identifiers.
-        drp.add_argument("--x_data_canc_file", nargs=2, action="append", metavar=("FILE", "IDENTIFIERS"), 
-                         default=[],
-                         help="List of tuples, each tuple has two elements, the first element is the file name and the second element is a comma separated list of gene system identifiers. Example: 'cancer_copy_number.tsv', 'Ensembl,Entrez'") 
-        drp.add_argument("--canc_col_name", type=str, default="improve_sample_id" , 
-                         help="Column name in the y data file (reponse) that represents the cancer sample identifier. Default is improve_sample_id.")
-       
-        drp.add_argument("--drug_col_name", type=str, default="improve_chem_id" , help="Column name in the y data file (reponse) that represents the drug identifier. Default is improve_chem_id.")
-
-
+        
 
 
 
