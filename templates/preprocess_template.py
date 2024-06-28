58cd8fe1b56a07cfcef8b1d7f74de0c8b9a2caa9
Oleksandr Narykov <alexandr.narykov@gmail.com>
2024-06-28 09:45:40 -0500

    Renamed return variable from parameter initialization

--- templates/preprocess_template.py
+++ templates/preprocess_template.py
@@ -122,14 +122,14 @@ def example_parameter_initialization_2():
 # main initializes parameters and calls run with the parameters
 # run is the main function that executes the script
 def main(args):
-    cfg1, logger1 = example_parameter_initialization_1()
-    cfg2, logger2 = example_parameter_initialization_2()
+    params1, logger1 = example_parameter_initialization_1()
+    params2, logger2 = example_parameter_initialization_2()
 
     # run task, passing params to run for backward compatibility, cfg could be used instead and contains the same information as params
     # in addition to the parameters, the cfg object provides access to the logger, the config object and data loaders
     # default is run(params)
-    status1 = run(cfg1, logger1)
-    status2 = run(cfg2, logger2)
+    status1 = run(params1, logger1)
+    status2 = run(params2, logger2)
 
 
 if __name__ == "__main__":
