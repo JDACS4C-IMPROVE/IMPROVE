db816790cb114e919e823e91c773e6737d407753
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-07-03 14:19:09 -0700

    Major cleaning of parameter definitions

--- improvelib/applications/drug_response_prediction/config.py
+++ improvelib/applications/drug_response_prediction/config.py
@@ -1,88 +1,93 @@
+"""
+The help section strings of certain parameters is prepended with the following brackets,
+encoding a suggested deprecation.
+"[Dep+]" -- parameters we should try to deprecate in the upcoming release
+"[Dep?]" -- parameters we might want to deprecate in the current or future release
+"[Dep]"  -- parameters we plan to keep in the upcoming release, but deprecate in the future
+"""
+
 from improvelib.initializer.stage_config import PreprocessConfig, TrainConfig, InferConfig
 
 
 class DRPPreprocessConfig(PreprocessConfig):
 
     # App-specific params (App: monotherapy drug response prediction)
-    # Note! This list should not be modified (i.e., no params should added or
-    # removed from the list.
     #
     # There are two types of params in the list: default and required
     # default:   default values should be used
     # required:  these params must be specified for the model in the param file
     _preproc_params = [
-        {"name": "y_data_files",  # default
+        {"name": "y_data_files", # default expected
          "type": str,
          "help": "List of files that contain the y (prediction variable) data. \
                 Example: [['response.tsv']]",
          },
-        {"name": "x_data_canc_files",  # required
+        {"name": "x_data_canc_files", # required
          "type": str,
          "help": "List of feature files including gene_system_identifer. Examples: \n\
                 1) [['cancer_gene_expression.tsv', ['Gene_Symbol']]] \n\
                 2) [['cancer_copy_number.tsv', ['Ensembl', 'Entrez']]].",
          },
-        {"name": "x_data_drug_files",  # required
+        {"name": "x_data_drug_files", # required
          "type": str,
          "help": "List of feature files. Examples: \n\
                 1) [['drug_SMILES.tsv']] \n\
                 2) [['drug_SMILES.tsv'], ['drug_ecfp4_nbits512.tsv']]",
          },
         {"name": "canc_col_name",
-         "default": "improve_sample_id",  # default
+         "default": "improve_sample_id", # default expected
          "type": str,
          "help": "Column name in the y (response) data file that contains the cancer sample ids.",
          },
-        {"name": "drug_col_name",  # default
+        {"name": "drug_col_name", # default expected
          "default": "improve_chem_id",
          "type": str,
          "help": "Column name in the y (response) data file that contains the drug ids.",
          },
-        # {"name": "data_format",  # default. Note! defined in improvelib
-        #  "default": ".parquet",
-        #  "type": str,
-        #  "help": "Format to load and save data",
-        #  },
-         # ---------------------------------------
-         {"name": "y_col_name",  # workflow. Note! moved from improvelib param defs
-          "type": str,
-          "default": "auc",
-          "help": "Column name in the y data file (e.g., response.tsv), that represents \
+        # ---------------------------------------
+        {"name": "y_col_name", # Note! moved from improvelib param defs
+         "type": str,
+         "default": "auc",
+         "help": "Column name in the y data file (e.g., response.tsv), that represents \
               the target variable that the model predicts. In drug response prediction \
               problem it can be IC50, AUC, and others."
-              },
+         }, 
+        # ---------------------------------------
+        # TODO y_data_suffix is currently used in utils.py (previously framework.py)
+        # We plan to hard-code this in the future release and deprecate. 
+        {"name": "y_data_suffix", # default expected
+         "type": str,
+         "default": "y_data", # TODO Oleksandr: "gives error when default is not set"
+         "help": "[Dep] Suffix to compose file name for storing true y dataframe."
+         },
 
     ]
 
     def __init__(self):
         super().__init__()
-        self.cli.set_command_line_options(options=self._preproc_params,
-                                          group='Drug Response Prediction Preprocessing')
+        self.cli.set_command_line_options(
+            options=self._preproc_params,
+            group='Drug Response Prediction Preprocessing')
 
 
 class DRPTrainConfig(TrainConfig):
     _app_train_params = [
-        # {"name": "data_format",  # default. Note! defined in improvelib
-        #  "type": str,
-        #  "help": "Format to load and save data",
-        #  },
-        {"name": "y_col_name",  # workflow. Note! moved from improvelib param defs
+        {"name": "y_col_name", # Note! moved from improvelib param defs
          "type": str,
-         "help": "Name of the metric to predict",
-         },
-        {"name": "y_data_suffix",  # default
-         "type": str,
-         # "default": "y_data", # TODO Oleksandr saya that when default is set, it gives error
-         "help": "Suffix for the columns in prediction file",
-         }
-         # ---------------------------------------
-         {"name": "y_col_name",  # workflow. Note! moved from improvelib param defs
-          "type": str,
-          "default": "auc",
-          "help": "Column name in the y data file (e.g., response.tsv), that represents \
+         "default": "auc",
+         "help": "Column name in the y data file (e.g., response.tsv), that represents \
               the target variable that the model predicts. In drug response prediction \
               problem it can be IC50, AUC, and others."
-              },
+         },
+        # ---------------------------------------
+        # TODO y_data_suffix is currently used in utils.py (previously framework.py)
+        # We plan to hard-code this in the future release and deprecate. 
+        {"name": "y_data_suffix", # default expected
+         "type": str,
+         "default": "y_data", # TODO Oleksandr: "gives error when default is not set"
+         "help": "[Dep] Suffix to compose file name for storing true y dataframe."
+         },
+
     ]
 
     def __init__(self):
@@ -94,47 +99,22 @@ class DRPTrainConfig(TrainConfig):
 
 class DRPInferConfig(InferConfig):
     _app_infer_params = [
-        # {"name": "data_format",  # default. Note! defined in improvelib
-        #          "type": str,
-        #          "help": "Format to load and save data",
-        #  },
-        {"name": "y_col_name",  # workflow. Note! moved from improvelib param defs
+        {"name": "y_col_name", # Note! moved from improvelib param defs
          "type": str,
-         "help": "Name of the metric to predict",
+         "default": "auc",
+         "help": "Column name in the y data file (e.g., response.tsv), that represents \
+              the target variable that the model predicts. In drug response prediction \
+              problem it can be IC50, AUC, and others."
          },
-        {"name": "y_data_suffix",  # default
+        # ---------------------------------------
+        # TODO y_data_suffix is currently used in utils.py (previously framework.py)
+        # We plan to hard-code this in the future release and deprecate. 
+        {"name": "y_data_suffix", # default expected
          "type": str,
-         # "default": "y_data", # TODO Oleksandr saya that when default is set, it gives error
-         "help": "Suffix for the columns in prediction file",
+         "default": "y_data", # TODO Oleksandr: "gives error when default is not set"
+         "help": "[Dep] Suffix to compose file name for storing true y dataframe."
          },
-        # {"name": "model_file_format",  # default
-        #  "type": str,
-        #  "help": "File format for the model",
-        #  },
-        # {"name": "model_file_name",  # default
-        #  "type": str,
-        #  "help": "File name for the model",
-        #  },
-        # {"name": "y_data_preds_suffix",  # default
-        #  "type": str,
-        #  "help": "???",
-        #  },
-        # {"name": "loss",  # default
-        #  "type": str,
-        #  "help": "???",
-        #  },
-        # {"name": "json_scores_suffix",  # default
-        #  "type": str,
-        #  "help": "???",
-        #  },
-         # ---------------------------------------
-        #  {"name": "y_col_name",  # workflow. Note! moved from improvelib param defs
-        #   "type": str,
-        #   "default": "auc",
-        #   "help": "Column name in the y data file (e.g., response.tsv), that represents \
-        #       the target variable that the model predicts. In drug response prediction \
-        #       problem it can be IC50, AUC, and others."
-        #       },
+
     ]
 
     def __init__(self):
--- improvelib/initializer/cli_params_def.py
+++ improvelib/initializer/cli_params_def.py
@@ -1,14 +1,13 @@
 """
-TODO
-Do we need all 4 lists of parameters?
-Determine which parameters we really need in CLI
-Do we want to rename any parameter
-
+The help section strings of certain parameters is prepended with the following brackets,
+encoding a suggested deprecation.
+"[Dep+]" -- parameters we should try to deprecate in the upcoming release
+"[Dep?]" -- parameters we might want to deprecate in the current or future release
+"[Dep]"  -- parameters we plan to keep in the upcoming release, but deprecate in the future
 """
 
-
-# Parameters that are relevant to all IMPROVE models
-# Defaults for these args are expected to be used
+# Parameters relevant to all IMPROVE model models
+# These parameters will be accessible in all model scripts (preprocess, train, infer)
 improve_basic_conf = [
     {"name": "log_level",
      "type": str,
@@ -18,189 +17,159 @@ improve_basic_conf = [
     {"name": "input_dir",
      "type": str,
      "default": "./",
-     "help": "Data dir name that stores the raw data, including x data, y data, and splits."
+     "help": "Dir containing input data for a given model script. The content \
+             will depend on the model script (preprocess, train, infer)."
     },
     {"name": "output_dir",
      "type": str,
      "default": "./",
-     "help": "Data dir name that stores the raw data, including x data, y data, and splits."
+     "help": "Dir where the outputs of a given model script will be saved. The \
+             content will depend on the model script (preprocess, train, infer)."
     },
     {"name": "config_file",
      "type": str,
      "default": None,
-     "help": "Configuration file for the script"
+     "help": "Configuration file for the model. The parameters defined in the \
+             file override the default parameter values."
      },
      # ---------------------------------------
-    {"name": "data_format",  # [Req] Must be specified for the model! TODO. rename to ml_data_format?
+    {"name": "data_format",  # [Req] depends on the DL framework that used by the model
      "type": str,
-     "default": ".parquet",
+     "default": ".parquet", # Note! this default assumes LGBM model
+     # "required": True, # TODO if this is required param, then remove default
      "help": "File format to save the ML data file (e.g., '.pt', '.tfrecords')",
      },
 ]
 
 # Parameters that are relevant to all IMPROVE preprocessing scripts
 improve_preprocess_conf = [
-    {"name": "x_data_dir",
+    {"name": "x_data_dir", # default expected
      "type": str,
      "default": "x_data",
      "help": "Dir name that contains the files with features data (x data)."
      },
-    {"name": "y_data_dir",
+    {"name": "y_data_dir", # default expected
      "type": str,
      "default": "y_data",
      "help": "Dir name that contains the files with target data (y data)."
      },
-    {"name": "splits_dir",
+    {"name": "splits_dir", # default expected
      "type": str,
      "default": "splits",
      "help": "Dir name that contains files that store split ids of the y data file."
      },
-    # Values for these args are expected to be passed:
-    # train_split_file, val_split_file, test_split_file
-    {"name": "train_split_file",  # workflow
-     "default": None,
+    # ---------------------------------------
+    {"name": "train_split_file",
      "type": str,
-     # "nargs": "+",
      "required": True,
-     "help": "The path to the file that contains the split ids (e.g., \
+     "help": "The path to the file that contains the train split ids (e.g., \
              'split_0_train_id', 'split_0_train_size_1024').",
      },
-    {"name": "val_split_file",  # workflow
-     "default": None,
+    {"name": "val_split_file",
      "type": str,
-     # "nargs": "+",
      "required": True,
-     "help": "The path to the file that contains the split ids (e.g., 'split_0_val_id').",
+     "help": "The path to the file that contains the val split ids (e.g., \
+             'split_0_val_id').",
      },
-    {"name": "test_split_file",  # workflow
-     "default": None,
+    {"name": "test_split_file",
      "type": str,
-     # "nargs": "+",
      "required": True,
-     "help": "The path to the file that contains the split ids (e.g., 'split_0_test_id').",
+     "help": "The path to the file that contains the test split ids (e.g., \
+             'split_0_test_id').",
      },
     # ---------------------------------------
-    {"name": "ml_data_outdir",  # workflow # TODO. this was previously ml_data_outpath
+    {"name": "ml_data_outdir",  # TODO. we plan to use output_dir instead
      "type": str,
      "default": "./ml_data",
      "help": "[Dep+] Path to save ML data (data files that can be fet to the prediction model).",
      },
-    # ---------------------------------------
-    # {"name": "x_data_suffix",  # TODO. rename x_data_stage_fname_suffix?
-    #   "type": str,
-    #   "default": "data",
-    #   "help": "Suffix to compose file name for storing x data (e.g., ...)."
-    # },
-    {"name": "y_data_suffix",  # default # TODO. rename y_data_stage_fname_suffix?
-     "type": str,
-     "default": "y_data",
-     "help": "[Dep] Suffix to compose file name for storing true y dataframe."
-     },
 
 ]
 
-# Parameters that are relevant to all IMPROVE training scripts
+# Parameters relevant to all IMPROVE train scripts
 improve_train_conf = [
-    {"name": "train_ml_data_dir",  # workflow
-     # "action": "store",
+    {"name": "train_ml_data_dir",  # TODO we plan to use input_dir instead
      "type": str,
      "default": "./ml_data",
      "help": "[Dep+] Datadir where train data is stored."
      },
-    {"name": "val_ml_data_dir",  # workflow
-     # "action": "store",
+    {"name": "val_ml_data_dir",  # TODO we plan to use input_dir instead
      "type": str,
      "default": "./ml_data",
      "help": "[Dep+] Datadir where val data is stored."
      },
-    {"name": "model_outdir",  # workflow
+    {"name": "model_outdir",  # TODO we plan to use output_dir instead
      "type": str,
      "default": "./out_model",  # csa_data/models/
      "help": "[Dep+] Dir to save trained models.",
      },
-    # {"name": "model_params",
-    {"name": "model_file_name",  # default # TODO: this was previously model_file_name
+    # ---------------------------------------
+    {"name": "model_file_name",  # default expected
      "type": str,
-     # "default": "model.pt",
      "default": "model",
      "help": "[Dep?] Filename to store trained model (str is w/o file_format)."
      },
-    {"name": "model_file_format",  # [Req]
+    {"name": "model_file_format",  # [Req] depends on the DL framework used by the model
      "type": str,
-     "default": ".pt",
-     "help": "[Dep?]File format to save the trained model."
+     "default": ".pt", # Note! this default assumes PyTorch model
+     # "required": True, # TODO if this is required param, then remove default
+     "help": "[Dep?] File format to save the trained model."
      },
     # ---------------------------------------
-    {"name": "epochs",  # [Req]
+    {"name": "epochs",
      "type": int,
-#      "default": 20,
      "required": True,
      "help": "Training epochs."
      },
-    {"name": "batch_size",  # [Req]
+    {"name": "learning_rate",
+     "type": float,
+     "required": True,
+     "help": "Learning rate for the optimizer."
+     },     
+    {"name": "batch_size",
      "type": int,
-#      "default": 64,
      "required": True,
      "help": "Trainig batch size."
      },
-    {"name": "val_batch",  # [Req]
+    {"name": "val_batch",
      "type": int,
      "default": 64,
-     # "default": argparse.SUPPRESS,
      "help": "Validation batch size."
      },
-    {"name": "loss",  # [Req] used in compute_metrics. TODO. should be model-specific
+    # ---------------------------------------
+    {"name": "loss",  # TODO used in compute_metrics(), but probably can be removed
      "type": str,
      "default": "mse",
-     "help": "[Dep+] Loss metric."
+     "help": "[Dep?] Loss metric."
      },
-    # {"name": "optimizer",
-    #  "type": str,
-    #  "default": "adam",
-    #  "help": "Optimizer for backpropagation."
-    # },
-    {"name": "learning_rate",
-     "type": float,
-#      "default": 0.0001,
-     "required": True,
-     "help": "Learning rate for the optimizer."
-    # },
-    # {"name": "train_data_processed",
-    #  "action": "store",
-    #  "type": str,
-    #  "help": "Name of pytorch processed train data file."
-    # },
-    # {"name": "val_data_processed",
-    #  "action": "store",
-    #  "type": str,
-    #  "help": "Name of pytorch processed val data file."
-    # },
-    # {"name": "model_eval_suffix", # TODO: what's that?
-    # y_data_stage_preds_fname_suffix
-    {"name": "early_stop_metric",  # [Req] TODO. consider moving to app or model (with patience)
+    {"name": "early_stop_metric",  # [Req] TODO consider moving to app or model params (with patience)
      "type": str,
      "default": "mse",
      "help": "Prediction performance metric to monitor for early stopping during \
              model training (e.g., 'mse', 'rmse').",
      },
-    {"name": "patience",  # [Req] TODO. consider moving to app or model (with patience)
+    {"name": "patience",  # [Req] TODO consider moving to app or model params (with early_stop_metric)
      "type": int,
      "default": 20,
-     # "default": argparse.SUPPRESS,
-     "help": "Iterations to wait for validation metrics getting worse before \
-             stopping training.",
+     "help": "Iterations to wait for a validation metric to get worse before \
+             stop training.",
      },
-    {"name": "y_data_preds_suffix",  # default
+    # ---------------------------------------
+    # TODO. y_data_preds_suffix, json_scores_suffix, pred_col_name_suffix
+    # are currently used in utils.py (previously framework.py)
+    # We plan to hard-code these in the future release and deprecate.
+    # Defined in improve_train_conf and improve_infer_conf
+    {"name": "y_data_preds_suffix", # default expected
      "type": str,
      "default": "predicted",
-     "help": "[Dep] Suffix to use for name of file to store inference results."
+     "help": "[Dep] Suffix to use for file name that stores predictions."
      },
-    {"name": "json_scores_suffix",  # default
+    {"name": "json_scores_suffix", # default expected
      "type": str,
      "default": "scores",
-     "help": "[Dep] Suffix to use for name of file to store scores."
+     "help": "[Dep] Suffix to use for file name that stores scores."
      },
-    {"name": "pred_col_name_suffix", # also defined in improve_infer_conf
+    {"name": "pred_col_name_suffix", # default expected
      "type": str,
      "default": "_pred",
      "help": "[Dep] Suffix to add to a column name in the y data file to identify \
@@ -211,35 +180,45 @@ improve_train_conf = [
 
 ]
 
-# Parameters that are relevant to all IMPROVE testing scripts
+# Parameters relevant to all IMPROVE infer scripts
 improve_infer_conf = [
-    {"name": "test_ml_data_dir",  # workflow
-     # "action": "store",
+    {"name": "test_ml_data_dir", # TODO we plan to use input_dir instead
      "type": str,
      "default": "./ml_data",
      "help": "[Dep+] Datadir where test data is stored."
      },
-    {"name": "model_dir",  # workflow
+    {"name": "model_dir", # TODO we plan to use input_dir instead
      "type": str,
      "default": "./out_model",  # csa_data/models/
      "help": "[Dep+] Dir to save inference results.",
      },
-    {"name": "infer_outdir",  # workflow
+    {"name": "infer_outdir",  # TODO we plan to use output_dir instead
      "type": str,
-     "default": "./out_infer",  # csa_data/infer/
+     "default": "./out_infer",
      "help": "[Dep+] Dir to save inference results.",
      },
-    # {"name": "test_data_processed",  # TODO: is this test_data.pt?
-    #  "action": "store",
-    #  "type": str,
-    #  "help": "Name of pytorch processed test data file."
-    # },
-    {"name": "test_batch",
+    # ---------------------------------------
+    {"name": "test_batch", # TODO should this be infer_batch size?
      "type": int,
      "default": 64,
      "help": "Test batch size.",
      },
-    {"name": "pred_col_name_suffix", # also defined in improve_train_conf
+    # ---------------------------------------
+    # TODO. y_data_preds_suffix, json_scores_suffix, pred_col_name_suffix
+    # are currently used in utils.py (previously framework.py)
+    # We plan to hard-code these in the future release and deprecate.
+    # Defined in improve_train_conf and improve_infer_conf
+    {"name": "y_data_preds_suffix", # default expected
+     "type": str,
+     "default": "predicted",
+     "help": "[Dep] Suffix to use for file name that stores predictions."
+     },
+    {"name": "json_scores_suffix", # default expected
+     "type": str,
+     "default": "scores",
+     "help": "[Dep] Suffix to use for file name that stores scores."
+     },
+    {"name": "pred_col_name_suffix", # default expected
      "type": str,
      "default": "_pred",
      "help": "[Dep] Suffix to add to a column name in the y data file to identify \
@@ -251,8 +230,8 @@ improve_infer_conf = [
 ]
 
 
-# Combine improve configuration into additional_definitions
+# Combine improve param definition into additional_definitions
 cli_param_definitions = improve_basic_conf + \
-    improve_preprocess_conf + \
-    improve_train_conf + \
-    improve_infer_conf
+                        improve_preprocess_conf + \
+                        improve_train_conf + \
+                        improve_infer_conf
