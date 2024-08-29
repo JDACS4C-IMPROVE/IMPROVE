ed253de62cbfec626028f570b86f468622e6d9f5
priyanka9991 <pvasanthakumari@anl.gov>
2024-08-29 10:26:58 -0500

    Add files for Parsl CSA

---
+++ workflows/parsl_csa/csa_params_def.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+
+fdir = Path(__file__).resolve().parent
+required = None
+additional_definitions = [
+    {"name": "source_datasets",
+     "nargs" : "+",
+     "type": str,
+     "default": ['CCLE'],
+     "help": "source_datasets for cross study analysis"
+    },
+    {"name": "target_datasets",
+     "nargs" : "+",
+     "type": str,
+     "default": ["CCLE", "gCSI"],
+     "help": "target_datasets for cross study analysis"
+    },
+    {"name": "split",
+     "nargs" : "+",
+     "type": str,
+     "default": ['0'],
+     "help": "Split of the source datasets for CSA"
+    },
+    {"name": "only_cross_study",
+     "type": bool,
+     "default": False,
+     "help": "If only cross study analysis is needed"
+    },
+    {"name": "model_name",
+     "type": str,
+     "default": 'graphdrp', ## Change the default to LGBM??
+     "help": "Name of the deep learning model"
+    },
+    {"name": "epochs",
+     "type": int,
+     "default": 10,
+     "help": "Number of epochs"
+    },
+    {"name": "use_singularity",
+     "type": bool,
+     "default": True,
+     "help": "Do you want to use singularity image for running the model?"
+    },
+    {"name": "singularity_image",
+     "type": str,
+     "default": '',
+     "help": "Singularity image file of the model"
+    }
+    ]
