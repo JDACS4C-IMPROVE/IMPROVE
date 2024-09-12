d463e356aa09e0b1e9feb0f52b4c80162cb5374f
Andreas Wilke <wilke@anl.gov>
2024-09-12 11:20:59 -0500

    Importing parsl config object from file

--- improvelib/config/csa.py
+++ improvelib/config/csa.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import configparser
+import importlib
 import yaml
 import json
 from pathlib import Path
@@ -12,6 +13,8 @@ import improvelib.config.base as base
 
 BREAK=os.getenv("IMPROVE_DEV_DEBUG", None)
 
+logger = logging.getLogger(__name__)
+logger.setLevel(os.getenv("IMPROVE_LOG_LEVEL", "INFO"))
 
 class Config(base.Config):
     """Class to handle configuration files."""
@@ -24,6 +27,7 @@ class Config(base.Config):
         ### Set CSA specific command line options
         # 1. Get Standard Options group
         # This is the group with title 'Standard Options'
+        logger.setLevel(self.log_level)
 
         self.section = 'CSA'
 
@@ -34,7 +38,7 @@ class Config(base.Config):
                 break
         
         if not sog:
-            self.logger.error("Can't find Standard Options group")
+            logger.error("Can't find Standard Options group")
             sys.exit(1)
 
         sog.add_argument(
@@ -48,12 +52,21 @@ class Config(base.Config):
         )
 
         sog.add_argument(
+            '-wfc', '--workflow_config_file',
+            metavar='FILE',
+            dest="workflow_config_file",
+            type=str,
+            default=None,
+            help="This file contains the workflow configuration with source and target datasets and model hyperparameters. The file is in YAML or json format."
+        )
+
+        sog.add_argument(
             '-pc', '--parsl_config_file',
             metavar='FILE',
             dest="parsl_config_file",
             type=str,
             default=None,
-            help="List of GPU IDs to use."
+            help="Runtime config for parsl as a python module. Config is in parsl_config variable."
         )
 
 
@@ -76,12 +89,58 @@ class Config(base.Config):
         p = super().initialize_parameters(pathToModelDir=None,
                                     section=self.section,
                                     default_config=default_config,
-                                    additional_definitions=self._options,
+                                    additional_definitions=additional_definitions,
                                     )
 
         self.gpu_ids = self.cli.args.gpu_ids
         self.parsl_config_file = self.cli.args.parsl_config_file
+
+        if self.parsl_config_file:
+            self._load_parsl_config()
+
         
+
+    def _load_parsl_config(self):
+        # if the config file is a yaml file load it as yaml
+        if self.parsl_config_file.endswith('.yaml'):
+
+            # print error not supported and exit
+            logger.error("YAML format is not supported for parsl_config_file")
+            sys.exit(1)
+
+            with open(self.parsl_config_file, 'r') as f:
+                return yaml.safe_load(f)
+        # if the config file is a json file load it as json
+        elif self.parsl_config_file.endswith('.json'):
+
+            # print error not supported and exit
+            logger.error("JSON format is not supported for parsl_config_file")
+            sys.exit(1)
+
+            with open(self.parsl_config_file, 'r') as f:
+                return json.load(f)
+            
+        # if the config file is python file import it and assign the importet parsl_config to self.parsl_config
+        elif self.parsl_config_file.endswith('.py'):
+            # import the parsl_config from the file
+
+            # add the directory of the config file to the path
+            sys.path.append(os.path.dirname(self.parsl_config_file))
+
+            # get filename without extension
+            filename = os.path.basename(self.parsl_config_file).split('.')[0]
+            
+            # import the file
+
+            importet_parsl_config = importlib.import_module(filename)
+
+            # assign the parsl_config to self.parsl_config
+            self.parsl_config = importet_parsl_config.parsl_config
+
+        else:
+            logger.error("Unknown file format for parsl_config_file")
+            sys.exit(1)
+
 
 if __name__ == "__main__":
     cfg = Config()
