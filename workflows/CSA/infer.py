8c7af9aded0bf25bb34620dcf6a7cddd0863cf84
Andreas Wilke <wilke@anl.gov>
2024-10-22 14:55:04 -0500

    Infer steps

--- workflows/CSA/infer.py
+++ workflows/CSA/infer.py
@@ -20,6 +20,10 @@ from parsl.data_provider.files import File
 
 
 
+from common import make_call
+from common import make_path 
+
+
 
 import csa_params_def as CSA
 import improvelib.utils as frm
@@ -72,105 +76,7 @@ def preprocess(
     return call
     # return f"cd {output_dir} ; {script} {input_dir} {output_dir} | tee my_stdout.txt"
 
-# Create preprocess output relative to the input directory for the workflow
-def preprocess_config(
-        input_dir = None, 
-        output_dir = None, 
-        model = "", 
-        dataset = "",
-        target_dataset = "",
-        source_dataset = "",
-        split = "", 
-        model_dir = None,
-        ) :
-    """Create dataset specifc config."""
 
-    # Output dir is under output_dir/stage/model/source-target-dataset/split
-
-    if not input_dir:
-        raise FileNotFoundError("Input directory is not specified.")
-    if not output_dir:
-        output_dir = input_dir
-
-    # constant for the stage
-    stage = "preprocess"
-
-    # Check if the source and target datasets are specified
-    if not source_dataset or not target_dataset:
-        raise ValueError("Source and target datasets are not specified.")
-
-    # Create directory paths
-    pp_output_dir = os.path.join( output_dir , stage , model , "-".join([source_dataset, target_dataset]) , split)
-    pp_input_dir = os.path.join( input_dir ) 
-
-    # Create the output directory
-    if not os.path.exists(pp_output_dir):
-        logger.debug(f"Creating output directory: {pp_output_dir}")
-        os.makedirs(pp_output_dir, exist_ok=True)
-    if not os.path.exists(pp_input_dir):
-        logger.debug(f"Creating input directory: {pp_input_dir}")
-        os.makedirs(pp_input_dir, exist_ok=True)
-
-    if source_dataset == target_dataset:
-            # If source and target are the same, then infer on the test split
-            test_split_file = f"{source_dataset}_split_{split}_test.txt"
-    else:
-            # If source and target are different, then infer on the entire target dataset
-            test_split_file = f"{target_dataset}_all.txt"
-
-    # Preprocess  data
-    inputs = { 
-        "files": {
-            "train" : f"{source_dataset}_split_{split}_train.txt",
-            "val" : f"{source_dataset}_split_{split}_val.txt",
-            "test" : test_split_file},
-        "input_dir" : pp_input_dir,
-        "output_dir" : pp_output_dir,
-        "stdout" : os.path.join(pp_output_dir , "stdout.txt"),
-        "stderr" : os.path.join(pp_output_dir , "stderr.txt")
-        }
-       
-
-    return (inputs, pp_input_dir, pp_output_dir , os.path.join( pp_output_dir ,  "stderr.txt")  , os.path.join( pp_output_dir , "stdout.txt"))
-
-@bash_app
-def train( 
-    script: str = None, 
-    input_dir: str = None, 
-    output_dir: str = None, 
-    epochs: int = 1, 
-    column_name: str = None, 
-    learning_rate: float = 0.001, 
-    batch_size: int = 32 , 
-    conda_env: str = None,
-    stdout: str = "stdout.txt", 
-    stderr: str = "stderr.txt", 
-    inputs: Sequence[File] = [], 
-    outputs: Sequence[File] = [] ):    
-    """Train the model."""
-    call= "echo 'Training the model'"
-
-    prefix = f"START=$(date +%s) ; echo Start:\t$START ; "
-
-    if conda_env:
-        conda= f"conda_path=$(dirname $(dirname $(which conda))) ; source $conda_path/bin/activate {conda_env} ; "
-    else:
-        conda = ""
-
-    suffix = "STOP=$(date +%s) ; echo Duration:\t$((STOP-START)) seconds ; sleep 1"
-
-    cli = [ script, 
-           "--input_dir" , input_dir,
-           "--output_dir" , output_dir,
-           "--epochs" , epochs,
-           "--y_col_name" , column_name,
-           "--learning_rate" , learning_rate,
-           "--batch_size" , batch_size
-           ]
-
-    call = echo + " ".join(cli)
-
-    return call
 
 def _check_executable(script: str = None, 
                       model_dir: str = None , 
@@ -181,10 +87,10 @@ def _check_executable(script: str = None,
     model_dir = Path(model_dir) if model_dir else None
 
     if not model_dir:
-        script = "preprocess.sh"
+        script = "infer.sh"
     else:
         model_dir = Path(model_dir)
-        script = model_dir / "preprocess.sh"
+        script = model_dir / "infer.sh"
 
     # Check if the script is in search path
     if shutil.which(script):
@@ -206,8 +112,113 @@ def _check_executable(script: str = None,
     else:
         raise FileNotFoundError(f"Script {script} does not exist.")
 
-    logger.debug(f"Preprocessing script: {script}")
+    logger.debug(f"Infer script: {script}")
     return script
+
+@bash_app
+def infer(
+    script: str = None,
+    input_data_dir: str = None,
+    input_model_dir: str = None,
+    output_dir: str = None,
+    calc_infer_scores: bool = False,
+    y_col_name: str = None,
+    conda_env: str = None,
+    stdout: str = "stdout.txt",
+    stderr: str = "stderr.txt",
+    inputs: Sequence[File] = [],
+    outputs: Sequence[File] = []):
+    """Infer the model."""
+    call = "echo 'Inferencing the model'"
+    prefix = f"START=$(date +%s) ; echo Start:\t$START "
+
+    if conda_env:
+        conda= f"conda_path=$(dirname $(dirname $(which conda))) ; source $conda_path/bin/activate {conda_env} "
+    else:
+        conda = "echo no conda env provided"
+
+    suffix = "STOP=$(date +%s) ; echo Duration:\t$((STOP-START)) seconds ; sleep 1"
+
+   
+    # Create the command line interface for inference
+    cli = [ "time",
+              script,
+          "--input_data_dir" , input_data_dir,
+          "--input_model_dir" , input_model_dir,
+          "--output_dir" , output_dir,
+          "--calc_infer_scores" , calc_infer_scores,
+          "--y_col_name" , y_col_name
+     ]
+
+    call = " ;".join([prefix, conda, " ".join(cli), suffix])
+
+    logger.debug(f"Inference command: {call}")
+    return call
+
+
+# Create training output relative to the input directory for the workflow
+def infer_config(
+        input_dir = None, 
+        output_dir = None, 
+        model = "", 
+        dataset = "",
+        target_dataset = "",
+        source_dataset = "",
+        split = "", 
+        model_dir = None,
+        ) :
+    """Create dataset specifc config."""
+
+    # Output dir is under output_dir/stage/model/source-target-dataset/split
+
+    if not input_dir:
+        raise FileNotFoundError("Input directory is not specified.")
+    if not output_dir:
+        output_dir = input_dir
+
+    # constant for the stage
+    stage = "infer"
+
+    # Check if the source and target datasets are specified
+    if not source_dataset or not target_dataset:
+        raise ValueError("Source and target datasets are not specified.")
+
+    # Create directory paths
+    output_dir = make_path(base_dir=output_dir, stage=stage, model=model, source_dataset=source_dataset, target_dataset=target_dataset, split=split)
+    input_dir = make_path(base_dir=input_dir, stage="preprocess", model=model, source_dataset=source_dataset, target_dataset=target_dataset, split=split, make_dir=False)
+
+    # should come from future / output of training
+    trained_model_dir = None
+    try: 
+        model_dir = make_path(base_dir=input_dir, stage="train", model=model, source_dataset=source_dataset, target_dataset=target_dataset, split=split, make_dir=False)
+    except FileNotFoundError as e:
+        logger.warning(f"Model directory not found for {model} {source_dataset} {target_dataset} {split}")
+        try:
+            model_dir = make_path(base_dir=input_dir, stage="train", model=model, source_dataset=source_dataset, target_dataset=None, split="all", make_dir=False)
+        except FileNotFoundError as e:
+            logger.error(f"Model directory not found for {model} {source_dataset} {target_dataset} {split}")
+            logger.error(f"Model directory not found for {model} {source_dataset} {split}")
+            raise e
+    
+    if not model_dir:
+        raise FileNotFoundError("Model directory is not specified.")
+    
+
+    
+
+    # Train input  data
+    inputs = { 
+        "files": {
+            },
+        "input_dir" : input_dir,
+        "output_dir" : output_dir,
+        "model_dir" : trained_model_dir,
+        "stdout" : os.path.join(output_dir , "stdout.txt"),
+        "stderr" : os.path.join(output_dir , "stderr.txt")
+        }
+       
+
+    return inputs
 
 def workflow(config: csa.Config,
              model_config: dict = None, 
@@ -219,7 +230,7 @@ def workflow(config: csa.Config,
 
     
 
-    print(f"config: {config}")
+    logger.info(f"config: {config}")
 
     # Check if the model is specified in the configuration file and assign it to the models variable
     if "model" not in model_config:
@@ -234,65 +245,60 @@ def workflow(config: csa.Config,
     script = _check_executable(model_dir = config.model_dir, model_name = model_name)
   
     # Iterate over the models
-
-    preprocess_futures = []
+    infer_futures = []
 
     for model in models:
         logger.debug(f"Checking model {model}")
         if model == model_name or model_name == "all":
-            logger.debug(f"Preprocessing for model {model}")
+            logger.debug(f"Infer for model {model}")
             for source in config.source_datasets:
-                logger.info(f"Preprocessing dataset {source} for {model}")
                 for target in config.target_datasets:
                     for split in config.splits:
-                        print(output_dir)
-                        print(os.getcwd())
-                        logger.info(f"Preprocessing dataset {source} for {model} and {split}")
-                        (options, pp_input_dir, pp_output_dir , stdout_file , stderr_file ) = preprocess_config(input_dir=input_dir,
-                                                                           output_dir=output_dir, 
-                                                                           model=model,
-                                                                           source_dataset=source, 
-                                                                           target_dataset=target,
-                                                                           split=split)
-                        logger.debug(f"Preprocessing with {script} for {source} and {target} in {split}")
-                        future = preprocess(
-                            input_dir = options["input_dir"],
-                            output_dir = options["output_dir"],
-                            train_file = options["files"]["train"],
-                            val_file = options["files"]["val"],
-                            test_file = options["files"]["test"],
-                            column_name = config.y_col_name,
+                        logger.info(f"Infer {model} on dataset {source}-{target} and {split}")
+
+                        if model in model_config and source in model_config[model]:
+                            logger.debug(f"Model {model} and source {source} found in the configuration.")
+                        else:
+                            logger.error(f"Model {model} and source {source} not found in the configuration.")
+                            raise ValueError(f"Model {model} and source {source} not found in the configuration.")
+
+                        infer_options = infer_config(
+                            input_dir=output_dir, # input_dir is the output of the preprocess
+                            output_dir=output_dir, 
+                            model=model,
+                            source_dataset=source, 
+                            target_dataset=target,
+                            split=split)
+                        
+                        logger.debug(f"Training with {script} for {source} and {split}")
+                        future = infer(
                             script = script,
+                            input_data_dir = infer_options["input_dir"],
+                            input_model_dir = infer_options["model_dir"],
+                            output_dir = infer_options["output_dir"],
+                            calc_infer_scores = True,
+                            y_col_name = config.y_col_name,
                             conda_env = config.conda_env,
                             inputs = [
-                                File(options["input_dir"]),
-                                File("/".join([options["input_dir"], "splits" , options["files"]["train"]])),
-                                File("/".join([options["input_dir"], "splits" , options["files"]["val"]])),
-                                File("/".join([options["input_dir"], "splits" , options["files"]["test"]])),],
-                            outputs = [
-                                File(options["output_dir"]),
-                                File(os.path.join(options["output_dir"], "stderr.txt")),
-                                File(os.path.join(options["output_dir"], "stdout.txt"))
+                                File(infer_options["input_dir"]),
+                                File(infer_options["model_dir"]),
                             ],
-                            stderr = os.path.join(options["output_dir"], "stderr.txt"),
-                            stdout = os.path.join(options["output_dir"], "stdout.txt"),
+                            outputs = [
+                                File(infer_options["output_dir"]),  
+                                File(infer_options["stdout"]),
+                                File(infer_options["stderr"]),
+                            ],
+                            stderr = infer_options["stderr"],
+                            stdout = infer_options["stdout"],
                             )
-                        logger.debug(f"Preprocessing task {future.tid} submitted: {model} {source} {target} {split}")
-                        preprocess_futures.append(future)
-                       
+                        logger.debug(f"Inference task {future.tid} submitted: {model} {source} {target} {split}")
+                        infer_futures.append(future)
+                        
         else:
             logger.debug(f"Skipping model {model}")
             continue
 
-    # Wait for all the futures to complete
-    logger.info("Waiting for all the preprocessing tasks to complete.")
-    for future in preprocess_futures:
-        # print(future.__dict__)
-        print(future.outputs)
-        print(future.stderr)
-        print(future.result())
-
-    for future in preprocess_futures:
+    for future in infer_futures:
         for data in future.outputs:
             if data.done():
                 # print(data.result().url)
@@ -364,6 +370,10 @@ def main(config: csa.Config):
 
 if __name__ == "__main__":
     # Initialize the CLI
+
+    print(    make_call(["echo", "Hello", "World"], "IMPROVE"))
+    sys.exit(0)
+
     config = csa.Config()
     
     params = config.initialize_parameters()
