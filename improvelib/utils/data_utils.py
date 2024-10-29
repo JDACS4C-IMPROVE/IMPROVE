c9816b21f4dc98f167e59266795e507a528173ac
Alex Partin <25892264+adpartin@users.noreply.github.com>
2024-10-29 12:34:54 -0700

    create data_utils.py

--- improvelib/utils/data_utils.py
+++ improvelib/utils/data_utils.py
@@ -1,8 +1,8 @@
 """
-This module contains functions for data manipulation and evaluation, including saving dataframes and storing predictions.
+This module includes functionality for data manipulation and evaluation, including saving dataframes and model predictions.
 """
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import numpy as np
 import pandas as pd
