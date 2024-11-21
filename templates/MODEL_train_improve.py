1ba130760c0f26d61df204a127fb5b732da1f802
Sara Jones <91564739+jonesse3@users.noreply.github.com>
2024-11-21 15:18:14 -0500

    Update MODEL_train_improve.py
    add input_dir

--- templates/MODEL_train_improve.py
+++ templates/MODEL_train_improve.py
@@ -86,7 +86,8 @@ def run(params: Dict):
         y_pred=your_val_predicted, 
         stage="val",
         y_col_name=params["y_col_name"],
-        output_dir=params["output_dir"]
+        output_dir=params["output_dir"],
+        input_dir=params["input_dir"]
     )
 
     # --------------------------------------------------------------------
