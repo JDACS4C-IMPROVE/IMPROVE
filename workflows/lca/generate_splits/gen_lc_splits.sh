51745697cd34a928bc56c5cea9277a142e20aa1c
Rajeev Jain <rajeeja@gmail.com>
2025-04-01 11:34:58 -0500

    o LCA GitHub Actions

--- workflows/lca/generate_splits/gen_lc_splits.sh
+++ workflows/lca/generate_splits/gen_lc_splits.sh
@@ -3,7 +3,7 @@
 # Example: bash gen_lc_splits.sh ../../../csa_data/raw_data/y_data/response.tsv ../../../csa_data/raw_data/splits
 
 # LC_SIZES=7
-LC_SIZES=10
+# LC_SIZES=10
 # LC_SIZES=12
 
 echo "LC sizes: $LC_SIZES"
@@ -13,10 +13,11 @@ echo "LC sizes: $LC_SIZES"
 
 dpath=$1
 spath=$2
+lc_sizes=$3
 
 python generate_lc_splits.py \
     --data_file_path $dpath \
     --splits_dir $spath \
-    --lc_sizes $LC_SIZES \
+    --lc_sizes $lc_sizes \
     --min_size 1024 \
     --lc_step_scale log
