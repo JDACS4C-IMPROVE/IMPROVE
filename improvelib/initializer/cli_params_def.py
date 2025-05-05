f431d8d686112141e429d9bf7d35db479d18d437
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-05-05 15:01:36 -0400

    bug

--- improvelib/initializer/cli_params_def.py
+++ improvelib/initializer/cli_params_def.py
@@ -14,7 +14,7 @@ Variables:
 
 import argparse
 
-from improvelib.utils import StoreIfPresent, str2bool
+from improvelib.utils import str2bool
 from improvelib.utils_config import StoreIfPresent
 
 # Parameters relevant to all IMPROVE model models
