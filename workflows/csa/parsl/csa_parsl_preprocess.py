7f5968b40b9a2085f0b28585e110ba96e0e761e3
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-07-09 11:08:22 -0400

    call

--- workflows/csa/parsl/csa_parsl_preprocess.py
+++ workflows/csa/parsl/csa_parsl_preprocess.py
@@ -35,7 +35,7 @@ def preprocess(script_call = None, conda_env = None, stderr = "stderr.txt", stdo
     import logging 
     logger = logging.getLogger(__name__)
     # Prefix and activate the conda environment
-    prefix = f"START=$(date +%s) ; echo Start:\t$START ; conda_path=$(dirname $(dirname $(which conda))) ; source $conda_path/bin/activate {conda_env} ; {script_call}"
+    prefix = f"START=$(date +%s) ; echo Start:\t$START ; conda_path=$(dirname $(dirname $(which conda))) ; source $conda_path/bin/activate {conda_env} ; "
     SUFFIX=' ; STOP=$(date +%s) ; echo Duration:\t$((STOP-START)) seconds ; sleep 1'
     call = prefix + script_call + SUFFIX
     logger.debug(f"Preprocessing command: {call}")
