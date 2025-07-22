d20d1d25b9681a7910fdbadf0955354efd7eda93
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-07-22 11:29:37 -0400

    model env bug

--- workflows/randomize/swarm/randomize_swarm.py
+++ workflows/randomize/swarm/randomize_swarm.py
@@ -33,7 +33,7 @@ if params['swarm_prefix'] is not None:
     prefix = params['swarm_prefix']
 else:
     model_env = check_dir_path_or_model_scripts_dir(params['model_environment'], params['model_scripts_dir'])
-    prefix = f"conda_path=$(dirname $(dirname $(which conda))) ; source $conda_path/bin/activate {params['model_scripts_dir']}/{params['model_environment']} ; export PYTHONPATH=../../../../IMPROVE ; "
+    prefix = f"conda_path=$(dirname $(dirname $(which conda))) ; source $conda_path/bin/activate {model_env} ; export PYTHONPATH=../../../../IMPROVE ; "
 
 
 #Model scripts
