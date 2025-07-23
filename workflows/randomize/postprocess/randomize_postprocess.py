68b36ae6965b0f9eb41405668285a23ae1c3553c
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-07-23 13:27:18 -0400

    fix postprocess

--- workflows/randomize/postprocess/randomize_postprocess.py
+++ workflows/randomize/postprocess/randomize_postprocess.py
@@ -8,117 +8,88 @@ from sklearn.metrics import mean_absolute_error
 
 
 def main():
-    main_parser = argparse.ArgumentParser(add_help=True)
-    common_args = argparse.ArgumentParser(add_help=False)
-    common_args.add_argument('-i', '--input_dir', default='./')
-    common_args.add_argument('-o', '--output_dir', default='./results')
-    common_args.add_argument('-y', '--y_col_name', default='auc')
-    common_args.add_argument('-m', '--metric_type', default='regression')
-    subparsers = main_parser.add_subparsers()
-
-    parser_lca_scores = subparsers.add_parser('random_scores', parents=[common_args])
-    parser_lca_scores.set_defaults(func=lca_scores)
-
-    args = main_parser.parse_args()
-    args.func(**vars(args))
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input_dir', default='./')
+    parser.add_argument('-o', '--output_dir', default='./results')
+    parser.add_argument('-y', '--y_col_name', default='auc')
+    parser.add_argument('-m', '--metric_type', default='regression')
+    parser.add_argument('-p', '--data_file_prefix', default='')
+    args = parser.parse_args()
+    random_scores(**vars(args))
 
 
 
 
-def _file_prefix(model_name, dataset):
-    # Set the file name prefix
-    if model_name is not None:
-        if dataset is not None:
-            prefix = model_name + "_" + dataset + "_"
-        else:
-            prefix = model_name + "_"
-    else:
-        if dataset is not None:
-            prefix = dataset + "_"
-        else:
-            prefix = ""
-    return prefix
 
-
-def lca_scores(input_dir, output_dir, y_col_name, metric_type, **kwargs):
+def random_scores(input_dir, output_dir, y_col_name, metric_type, data_file_prefix, **kwargs):
     input_dir_path = Path(input_dir).resolve()  # absolute path to result dir
     output_dir = Path(output_dir)
     os.makedirs(output_dir, exist_ok=True)
     scores_fpath = output_dir / "all_scores.csv"
     missing_pred_files = []
-    
-
     dfs = []
 
     datasets = sorted(list(input_dir_path.glob("*")))
     datasets = [path for path in datasets if os.path.isdir(path)]
     print(datasets)
     for dataset in datasets:
+        dataset_name = os.path.basename(os.path.normpath(dataset))
         print(f"This dataset is {dataset}")
         split_types = sorted(list(dataset.glob("*")))
         split_types = [path for path in split_types if os.path.isdir(path)]
         print(split_types)
+        for split_type in split_types:
+            split_type_name = os.path.basename(os.path.normpath(split_type))
+            print(f"This split_type is {split_type}")
+            split_nums = sorted(list(split_type.glob("*")))
+            split_nums = [path for path in split_nums if os.path.isdir(path)]
+            for split_num in split_nums:
+                split_num_name = os.path.basename(os.path.normpath(split_num))
+                print(f"This split_num is {split_num}")
+                rands = sorted(list(split_num.glob("*")))
+                rands = [path for path in rands if os.path.isdir(path)]
+                for rand in rands:
+                    rand_name = os.path.basename(os.path.normpath(rand))
+                    preds_file_path = rand / "test_y_data_predicted.csv"
+                    try:
+                        columns_to_load = [f"{y_col_name}_true", f"{y_col_name}_pred"]
+                        preds = pd.read_csv(preds_file_path, sep=',', usecols=columns_to_load)
+                        # Compute scores
+                        y_true = preds[f"{y_col_name}_true"].values
+                        y_pred = preds[f"{y_col_name}_pred"].values
+                        scores = compute_metrics(y_true, y_pred, metric_type=metric_type)
+                        scores_df = pd.DataFrame(scores)
+                        scores_df['dataset'] = dataset_name
+                        scores_df['split_type'] = split_type_name
+                        scores_df['split_num'] = split_num_name
+                        scores_df['rand'] = rand_name
+                        if scores_df.empty is False:
+                            dfs.append(scores_df)
+                        # Clean
+                        del preds, y_true, y_pred, scores_df
 
+                    except FileNotFoundError:
+                        print(f"Error: File not found! {preds_file_path}")
+                        missing_pred_files.append(preds_file_path)
 
-    """
-    for dir_path in dirs:
-        split_num = str(dir_path.name).split("_")[1]
-        shard_dirs = sorted(list((dir_path).glob(f"sz_*")))
-        shard_score_dict = {}  # dict (key: split id, value: dict of scores)
-        for shard_dir in shard_dirs:
-            preds_file_path = shard_dir / "test_y_data_predicted.csv"
-            try:
-                columns_to_load = [f"{y_col_name}_true", f"{y_col_name}_pred"]
-                preds = pd.read_csv(preds_file_path, sep=',',
-                                    usecols=columns_to_load)
-
-                # Compute scores
-                y_true = preds[f"{y_col_name}_true"].values
-                y_pred = preds[f"{y_col_name}_pred"].values
-                sc = compute_metrics(y_true, y_pred, metric_type=metric_type)
-                sc['mae'] = mean_absolute_error(y_true, y_pred)
-                shard = int(shard_dir.name.split("sz_")[1])
-                shard_score_dict[shard] = sc
-                # Clean
-                del preds, y_true, y_pred, sc, shard
-
-            except FileNotFoundError:
-                print(f"Error: File not found! {preds_file_path}")
-                missing_pred_files.append(preds_file_path)
-
-            except Exception as e:
-                print(f"An unexpected error occurred: {e}")
-
-        # Convert dict to df, and aggregate dfs
-        shard_score_df = pd.DataFrame(shard_score_dict)
-        shard_score_df = shard_score_df.stack().reset_index()
-        shard_score_df.columns = ['metric', 'shard', 'value']
-        shard_score_df['split'] = split_num
-        if shard_score_df.empty is False:
-            dfs.append(shard_score_df)
-
-    filename = _file_prefix(model_name, dataset) + "all_scores.csv"
-
+                    except Exception as e:
+                        print(f"An unexpected error occurred: {e}")  
+    
+    filename = data_file_prefix + "all_scores.csv"
     # Concat dfs and save
     if not dfs:
-        print("No runtimes found.")
+        print("No data found.")
     else:
         scores = pd.concat(dfs, axis=0)
-        #scores['model'] = model_name
-        if model_name is not None:
-            scores['model'] = model_name
-        if dataset is not None:
-            scores['dataset'] = dataset
         scores.to_csv(output_dir / filename, index=False)
         del dfs
 
-        missing_preds_filename = _file_prefix(model_name, dataset) + "missing_pred_files.txt"
-        if len(missing_pred_files) > 0:
-            with open(f"{output_dir}/{missing_preds_filename}", "w") as f:
-                for line in missing_pred_files:
-                    line = 'infer' + str(line).split('infer')[1]
-                    f.write(line + "\n")
-    """
+    missing_preds_filename = data_file_prefix + "missing_pred_files.txt"
+    if len(missing_pred_files) > 0:
+        with open(f"{output_dir}/{missing_preds_filename}", "w") as f:
+            for line in missing_pred_files:
+                f.write(line + "\n")
+
 
     
 
