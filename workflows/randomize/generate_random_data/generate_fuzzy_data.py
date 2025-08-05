9b5e59452d3cf4c3ad1b353c3df8de7b3c514931
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-08-05 10:53:53 -0400

    testing

--- workflows/randomize/generate_random_data/generate_fuzzy_data.py
+++ workflows/randomize/generate_random_data/generate_fuzzy_data.py
@@ -11,7 +11,7 @@ import argparse
 def _get_count_df(response_df, feature_df, id_col):
     #response_df = response_df[response_df["source"] == "GDSCv1"]
     response_count = response_df[id_col].value_counts() #here
-    response_count = pd.DataFrame(response_count)
+    response_count = pd.DataFrame(response_count, columns=[id_col, 'count'])
     print("response_count:", response_count)
     response_count_withfeature = response_count.join(feature_df, how='left').reset_index()
     return response_count_withfeature
