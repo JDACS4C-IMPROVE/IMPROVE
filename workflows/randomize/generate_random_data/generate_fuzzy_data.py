f672a93557e54f0160d38ca82912d9fd5b30acb3
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-08-05 12:32:31 -0400

    testing

--- workflows/randomize/generate_random_data/generate_fuzzy_data.py
+++ workflows/randomize/generate_random_data/generate_fuzzy_data.py
@@ -42,7 +42,7 @@ def _get_single_fuzzy(df_row, id_col, randomize, percent):
     cols_fuzzy = []
     cols_fuzzy = cols_fuzzy + [pd.Series(names)]
     for g in range(2, df_row.size):
-        val = df_row[[g]]
+        val = df_row.iloc[g]
         print("g: ", g)
         print("val: ", val)
         new_val = _randomize_GE(val, count, randomize, percent)
