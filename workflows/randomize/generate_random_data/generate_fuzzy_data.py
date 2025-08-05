53eb4df63984c74c99b6c64f600eea1241d7c06b
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-08-05 11:51:31 -0400

    testing

--- workflows/randomize/generate_random_data/generate_fuzzy_data.py
+++ workflows/randomize/generate_random_data/generate_fuzzy_data.py
@@ -135,21 +135,15 @@ def post_shuffle_data(df, strategy, seed=42):
     return df_copy
 
 def save_df(df, path):
-    import polars as pl
-    print("Saving with Polars.")
-    pl_df = pl.from_pandas(df)
-    print(pl_df)
-    pl_df.write_csv(path, separator='\t')
-    '''
     try:
         import polars as pl
         print("Saving with Polars.")
-        pl_df = pl.from_pandas(df)
+        pl_df = pl.from_pandas(df.reset_index())
         pl_df.write_csv(path, separator='\t')
     except:
         print("Polars not present. Using pandas to save.")
         df.to_csv(str(path), sep='\t')
-    '''
+
 
 
 def main():
