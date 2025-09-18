89914ca222b238128d1c13f8cc3419ae53815d4e
Alex Partin <25892264+adpartin@users.noreply.github.com>
2025-09-18 15:31:29 -0500

    comments

--- workflows/hpo/deephyper/hpo_deephyper_subprocess_train.sh
+++ workflows/hpo/deephyper/hpo_deephyper_subprocess_train.sh
@@ -16,7 +16,7 @@ conda_path=$(dirname $(dirname $(which conda)))
 source $conda_path/bin/activate $CONDA_ENV
 echo "Activated conda env $CONDA_ENV"
 
-# get mandatory arguments
+# Get mandatory arguments
 SCRIPT=$2
 INPUT_DIR=$3
 OUTPUT_DIR=$4
@@ -24,23 +24,20 @@ CUDA_VISIBLE_DEVICES=$5
 
 command="python $SCRIPT --input_dir $INPUT_DIR --output_dir $OUTPUT_DIR"
 
-
-# append hyperparameter arguments to python call
+# Append hyperparameter arguments to python call
 for i in $(seq 6 $#)
 do
     if [ $(($i % 2)) == 0 ]; then
-        command="${command} --${!i}"
+        command="${command} --${!i}" # param
     else
-        command="${command} ${!i}"
+        command="${command} ${!i}" # value
     fi
 done
 
-
 echo "command: $command"
 
-# run python script
+# Run python script
 CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} $command
-
 
 source $conda_path/bin/deactivate
 echo "Deactivated conda env $CONDA_ENV"
